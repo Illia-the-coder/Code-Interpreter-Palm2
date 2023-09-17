5b0d74e4b62e2f3f7f726f898d97b89e7bba8a0f
Liu <Illia56@users.noreply.huggingface.co>
2023-09-17 07:14:42 +0000

    Update app.py

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ if "messages" not in st.session_state:
 
 # Display chat messages from history on app rerun
 for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
+    with st.chat_message(message["role"], avatar=("🧑‍💻" if message["role"]=='human' else '🌴')):
         st.markdown(message["content"])
         
 # React to user input

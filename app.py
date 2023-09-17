4cde0cdad849bf11a99b2335fa7f818bd5905cec
Liu <Illia56@users.noreply.huggingface.co>
2023-09-17 07:04:26 +0000

    Update app.py

--- app.py
+++ app.py
@@ -62,14 +62,11 @@ if prompt := st.chat_input("Ask Palm 2 anything..."):
         st.markdown(response['content'])
     
     if response['code']:
-        # if st.button('Run Code'):
-        with st.status("Running code..."):
-            try:
-                exec(response['code'])
-            except Exception as e:
-                st.write(f"ERROR {e}...")
-                    
-            
-            
+        if st.button('Run Code'):
+            with st.status("Running code..."):
+                try:
+                    exec(response['code'])
+                except Exception as e:
+                    st.write(f"ERROR {e}...")
     # Add assistant response to chat history
     st.session_state.messages.append({"role": "assistant", "content": response['content']})

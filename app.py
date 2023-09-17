b60058dcc7e8c2ebb7628cd6795d7ae969ec4a13
Liu <Illia56@users.noreply.huggingface.co>
2023-09-17 18:22:26 +0000

    Update app.py

--- app.py
+++ app.py
@@ -24,6 +24,7 @@ with st.sidebar:
     selected_language_name = st.sidebar.selectbox("Select Language", list(GOOGLE_LANGUAGES_TO_CODES.keys()))
     code_interpreter = st.sidebar.toggle("Code Interpreter", value=False)
     system_prompt = st.sidebar.text_input("System prompt for code interpreter", value = "Rule 1: If a user requests a code snippet, provide only one that can run in a Streamlit app without requiring additional libraries.")
+    useSystemPrompt = st.sidebar.toggle("Use System prompt", value=True)
     
 # Retrieve the corresponding language code from the dictionary
 selected_language_code = GOOGLE_LANGUAGES_TO_CODES[selected_language_name]
@@ -43,7 +44,7 @@ st.write(DESCRIPTION)
 def predict(message):
     with st.status("Requesting Palm-2🌴..."):
         st.write("Requesting API...")
-        response = bard.get_answer(message if not code_interpreter else message + system_prompt)
+            response = bard.get_answer(message if not (code_interpreter and useSystemPrompt) else message + system_prompt)
         st.write("Done...")
         
         st.write("Checking images...")
